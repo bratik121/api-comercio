bd5750803e965d0290603ba39d1427ea63e9a445
Bryant <thiswasbrii@gmail.com>
2025-04-26 15:45:01 -0400

    🚧Feat in progress: adding user controller

---
+++ src/user/infraestructure/controller/user.controller.ts
@@ -0,0 +1,116 @@
+import { Body, Controller, Inject, Post } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiConflictResponse,
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { IIdGen } from 'src/common/aplication/id-gen/id-gen.interfaces';
+import { IMapper } from 'src/common/aplication/mappers/mapper.interface';
+import { UuidGen } from 'src/common/infraestructure/id-gen/uuid-gen';
+import { User } from 'src/user/domain/user';
+import { OdmUserEntity } from '../entities/odm-entities';
+import { OdmUserMapper } from '../mappers/odm-mapper/odm-user.mapper';
+import { OrmUserMapper } from '../mappers/orm-mapper/orm-user.mapper';
+import { OrmUserEntity } from '../entities/orm-entities';
+import { InjectEntityManager } from '@nestjs/typeorm';
+
+import {
+  IOdmUserRepository,
+  IOrmUserRepository,
+} from 'src/user/domain/repositories';
+import { IService } from 'src/common/aplication/services/IServices';
+import { CreateUserReponse, CreateUserRequest } from 'src/user/aplication/dtos';
+import { IEncryptor } from 'src/common/aplication/encryptor/encryptor.interface';
+import { BcryptEncryptor } from 'src/common/infraestructure/encryptor/bcrypt';
+import { IEventPublisher } from 'src/common/aplication/events/event-publisher.interfaces';
+import { IEventSubscriber } from 'src/common/aplication/events/event-suscriber.interface';
+import { UserRegisteredEvent } from 'src/user/domain/events';
+import { EntityManager } from 'typeorm';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { OdmUserRepository } from '../repositories/odm-repositories/odm-user.repository';
+import { OrmUserRepository } from '../repositories/orm-repositories/orm-user.repository';
+import { OdmSaveUserEvent } from '../events';
+import { CreateUserService } from 'src/user/aplication/services';
+import { RegisteredUserMapper } from '../mappers/domain-event-mapper';
+import { RabbitMQEventPublisher } from 'src/common/infraestructure/events/publishers/rabbittMq.publisher';
+import { CreateUserDto } from '../dtos';
+
+@ApiTags('User')
+@Controller('user')
+export class UserController {
+  //?Id generator
+  private readonly genId: IIdGen = new UuidGen();
+  //? Encripter
+  private readonly encryptor: IEncryptor = new BcryptEncryptor();
+
+  //? Publisher
+  private readonly _eventPublisher: IEventPublisher;
+
+  //?Mappers
+  private readonly _odmUserMapper: IMapper<User, OdmUserEntity> =
+    new OdmUserMapper();
+  private readonly _ormUserMapper: IMapper<User, OrmUserEntity> =
+    new OrmUserMapper();
+
+  //?Repositories
+  private readonly _odmUserRepository: IOdmUserRepository;
+  private readonly _ormUserRepository: IOrmUserRepository;
+
+  //?Services
+  private createUserService: IService<CreateUserRequest, CreateUserReponse>;
+
+  //?Events
+  private readonly _saveUserEvent: IEventSubscriber<UserRegisteredEvent>;
+
+  constructor(
+    @InjectEntityManager() private readonly entityManager: EntityManager,
+    @InjectModel('user') userModel: Model<OdmUserEntity>,
+  ) {
+    //* Repositories
+    this._odmUserRepository = new OdmUserRepository(
+      userModel,
+      this._odmUserMapper,
+    );
+    this._ormUserRepository = new OrmUserRepository(
+      this.entityManager,
+      this._ormUserMapper,
+    );
+
+    //*Services
+    this.createUserService = new CreateUserService(
+      this._odmUserRepository,
+      this._ormUserRepository,
+      this._eventPublisher,
+      this.encryptor,
+      this.genId,
+    );
+
+    //*Events
+    this._saveUserEvent = new OdmSaveUserEvent(this._odmUserRepository);
+
+    //* Suscribe to events
+    this._eventPublisher.subscribe(
+      'UserRegistedEvent',
+      [this._saveUserEvent],
+      RegisteredUserMapper,
+    );
+  }
+
+  @Post('create')
+  @ApiCreatedResponse({ description: 'User created succesfully' })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
+  @ApiConflictResponse({ description: 'Conflict' })
+  @ApiNotFoundResponse({ description: 'Not Found' })
+  async createUser(@Body() body: CreateUserDto) {
+    const request = new CreateUserRequest(body.name, body.email, body.password);
+
+    const response = await this.createUserService.execute(request);
+    if (response.isSuccess()) {
+      return response.getValue().dataToString();
+    }
+    return response;
+  }
+}
